5543da83cee034df097f3b30d8025b243765829b
rijash-ali <rijash.raka@gmail.com>
2023-09-17 15:18:35 +0530

    Completed: drugTransfer contract

--- chaincode/contracts/drugTransfer.contract.js
+++ chaincode/contracts/drugTransfer.contract.js
@@ -1,15 +1,24 @@
 'use strict';
 
 import { Contract } from "fabric-contract-api";
-import { DRUG_TRANSFER_CONTRACT_KEY, deriveDrugAssetKey, deriveDrugPurchaseAssetKey, pharmaNameSpaces } from "../utils/assetKeys";
+import { DRUG_TRANSFER_CONTRACT_KEY, deriveDrugAssetKey, deriveDrugPurchaseAssetKey, deriveDrugShipmentAssetKey, pharmaNameSpaces } from "../utils/assetKeys";
 import { ContractRepository } from "../repository";
-import { PharmaNetOrgs, ShipmentStatus } from "../utils/enums";
+import { PharmaNetOrgs, PharmaNetRoles, ShipmentStatus } from "../utils/enums";
 
 export class DrugTransferContract extends Contract {
   constructor() {
     super(DRUG_TRANSFER_CONTRACT_KEY);
   }
 
+  /**
+   * Create a new purchase order on the network
+   * @param ctx - The transaction context object
+   * @param buyerCRN - Company Registration Number of buyer
+   * @param sellerCRN - Company Registration Number of seller
+   * @param drugName - Name of the drug
+   * @param quantity - Quantity of the drug requested
+   * @returns
+   */
   async createPO(ctx, buyerCRN, sellerCRN, drugName, quantity) {
     /** Role: Restricted to Distributor | Retailer */
     if ([PharmaNetOrgs.distributor, PharmaNetOrgs.retailer].includes(ctx.clientIdentity.getMSPID()))
@@ -50,6 +59,15 @@ export class DrugTransferContract extends Contract {
     return asset;
   }
 
+  /**
+   * Create a new shipment on the network
+   * @param ctx - The transaction context object
+   * @param buyerCRN - Company Registration Number of buyer
+   * @param drugName - Name of the drug
+   * @param listOfAssets - List of serial numbers of drugs being delivered
+   * @param transporterCRN - Company Registration Number of transporter delivering the shipment
+   * @returns
+   */
   async createShipment(ctx, buyerCRN, drugName, listOfAssets = [], transporterCRN) {
     /** Role: Restricted to Distributor | Retailer */
     if ([PharmaNetOrgs.distributor, PharmaNetOrgs.retailer].includes(ctx.clientIdentity.getMSPID()))
@@ -102,5 +120,88 @@ export class DrugTransferContract extends Contract {
     };
 
     await ContractRepository.putAsset(key, asset);
+  };
+
+  /**
+   * Update shipment once it is delivered
+   * @param ctx - The transaction context object
+   * @param buyerCRN - Company Registration Number of buyer
+   * @param drugName - Name of the drug
+   * @param transporterCRN - Company Registration Number of transporter delivering the shipment
+   * @returns
+   */
+  async updateShipment(ctx, buyerCRN, drugName, transporterCRN) {
+    /** Role: Restricted to Transporter */
+    if (ctx.clientIdentity.getMSPID() !== PharmaNetOrgs.transporter)
+      throw new Error("Only transporters are allowed to update shipments on pharma network!");
+
+
+    const key = deriveDrugShipmentAssetKey(ctx, buyerCRN, drugName);
+
+    const consignment = await ContractRepository.getAsset(key);
+    if (!consignment)
+      throw new Error("Invalid consignment.");
+    else if (consignment.status !== ShipmentStatus.inTransit)
+      throw new Error("Consignment is already delivered.");
+
+    /**
+     * Validations:
+     * Does the buyer exist for the buyerCRN?
+     * Is the consignment intended for the buyer? 
+     */
+    const buyer = await ContractRepository.getFirstAssetFromKeyPrefix(ctx, pharmaNameSpaces.commpanyAsset, buyerCRN);
+    if (!buyer || !consignment.shipmentID.includes(buyerCRN))
+      throw new Error(`Invalid buyer: ${buyerCRN}`);
+
+    const transporter = await ContractRepository.getFirstAssetFromKeyPrefix(ctx, pharmaNameSpaces.commpanyAsset, transporterCRN);
+
+    /**
+     * Validations:
+     * Does the transporter exist for the transporterCRN with role Transporter?
+     * Is the consignment assigned to the transporter? 
+     */
+    if (!transporter || transporter.value.hierarchyKey !== PharmaNetRoles.Transporter || consignment.transporter !== transporter.key)
+      throw new Error(`Invalid transporter: ${transporterCRN}`);
+
+    /** Refactor using Rich queries from Couch Db */
+    consignment.assets.map(async drugAssetKey => {
+      const drugAsset = await ContractRepository.getAsset(drugAssetKey);
+      const updatedDrugAsset = {
+        ...drugAsset,
+        owner: buyer.key,
+        shipment: [...drugAsset.shipment, key],
+      };
+      await ContractRepository.putState(drugAssetKey, updatedDrugAsset);
+    });
+
+    const updatedAsset = { ...consignment, status: ShipmentStatus.delivered };
+    await ContractRepository.putState(key, updatedAsset);
+
+    return updatedAsset;
+  };
+
+  async retailDrug(ctx, drugName, serialNo, retailerCRN, customerAadhar) {
+    const retailer = await ContractRepository.getFirstAssetFromKeyPrefix(ctx, pharmaNameSpaces.commpanyAsset, retailerCRN);
+
+    /** Role: Restricted to Retailer */
+    if (ctx.clientIdentity.getMSPID() !== PharmaNetOrgs.retailer || !retailer || retailer.hierarchyKey !== PharmaNetRoles.Retailer)
+      throw new Error("Invalid seller: Only retailers can sell to customers");
+
+    const drugAssetKey = deriveDrugAssetKey(ctx, drugName, serialNo);
+    const drugAsset = await ContractRepository.getAsset(drugAssetKey);
+    if (!drugAsset)
+      throw new Error("Drug asset is not registered on the network");
+
+    /** Validation: Retailer should be the current owner of the asset */
+    if (drugAsset.owner !== retailer.companyID)
+      throw new Error(`The drug with serialNo ${serialNo} is not owned by the seller.`);
+
+    const updatedAsset = {
+      ...drugAsset,
+      owner: customerAadhar
+    };
+    await ContractRepository.putState(drugAssetKey, updatedAsset);
+
+    return updatedAsset;
   };
 }
